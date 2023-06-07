069c534907743519b01c761edb28fa145ee44ed4
The-Flying-Dev <leandranm@outlook.com>
2023-06-07 23:15:22 +0200

    map letters of a string

--- 05 Map the letters of a string.rb
+++ 05 Map the letters of a string.rb
@@ -3,5 +3,13 @@
 # For example, if the parameter is ‘dodo,’ the function should return “{ :d=>[0, 2], :o=>[1, 3] }”.
 
 
-def mapping_letters
-end
+def letters_index(str)
+
+    str.each_char
+       .with_index
+       .with_object({}) { |(char, index), hash| (hash[char] ||= []) << index }
+
+end 
+
+
+p letters_index("function that accepts a word as a parameter")
